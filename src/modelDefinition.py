25d6405fd90540727f096a6e7c17bf88e938265d
nsykim <noahsykim@gmail.com>
2024-10-09 13:58:30 -0400

    changed class name to RuleBasedModel from LPModel

--- src/modelDefinition.py
+++ src/modelDefinition.py
@@ -3,7 +3,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-class LPModel:
+class RuleBasedModel:
     """
     Logic programming model
     """
