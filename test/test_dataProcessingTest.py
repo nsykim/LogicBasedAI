77cd8366ee621904b6bdb079f3f24b1c6432f9e4
nsykim <noahsykim@gmail.com>
2024-10-10 17:38:54 -0400

    implemented unit testing and end to end testing

--- test/test_dataProcessingTest.py
+++ test/test_dataProcessingTest.py
@@ -6,6 +6,7 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
 from dataProcessing import DataProcessing
+
 class TestDataProcessing(unittest.TestCase):
 
     def setUp(self):
@@ -19,38 +20,64 @@ class TestDataProcessing(unittest.TestCase):
         28,Private,Bachelors,Married-civ-spouse,Prof-specialty,Wife,Black,Female,40,>50K
         """)
         self.data_processing = DataProcessing(self.csv_data)
-
-    def test_load_data(self):
         self.data_processing.load_data()
+
+    # Unit Tests
+    def test_load_data(self):
         self.assertIsInstance(self.data_processing.data, pd.DataFrame)
         self.assertEqual(len(self.data_processing.data), 5)
 
     def test_detect_categorical_columns(self):
-        self.data_processing.load_data()
         categorical_columns = self.data_processing.detect_categorical_columns()
         expected_categorical_columns = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'income']
         self.assertListEqual(categorical_columns, expected_categorical_columns)
 
     def test_preprocess(self):
-        self.data_processing.load_data()
         preprocessed_data = self.data_processing.preprocess()
         self.assertIsInstance(preprocessed_data, pd.DataFrame)
         self.assertFalse(preprocessed_data.isnull().values.any())
         self.assertTrue(all(preprocessed_data.dtypes != 'object'))
 
     def test_split_data(self):
-        self.data_processing.load_data()
         self.data_processing.preprocess()
         X_train, X_test, y_train, y_test = self.data_processing.split_data(target_col='income')
         self.assertEqual(len(X_train) + len(X_test), len(self.data_processing.data))
         self.assertEqual(len(y_train) + len(y_test), len(self.data_processing.data))
 
     def test_balance_data(self):
-        self.data_processing.load_data()
         self.data_processing.preprocess()
         balanced_data = self.data_processing.balance_data(target_col='income')
         self.assertIsInstance(balanced_data, pd.DataFrame)
         self.assertTrue('income' in balanced_data.columns)
+        self.assertEqual(balanced_data['income'].value_counts().iloc[0], balanced_data['income'].value_counts().iloc[1])
+
+    # End-to-End Tests
+    def test_end_to_end_workflow(self):
+        # Load data
+        self.data_processing.load_data()
+        self.assertIsInstance(self.data_processing.data, pd.DataFrame)
+
+        # Detect categorical columns
+        categorical_columns = self.data_processing.detect_categorical_columns()
+        expected_categorical_columns = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'income']
+        self.assertListEqual(categorical_columns, expected_categorical_columns)
+
+        # Preprocess data
+        preprocessed_data = self.data_processing.preprocess()
+        self.assertIsInstance(preprocessed_data, pd.DataFrame)
+        self.assertFalse(preprocessed_data.isnull().values.any())
+        self.assertTrue(all(preprocessed_data.dtypes != 'object'))
+
+        # Split data
+        X_train, X_test, y_train, y_test = self.data_processing.split_data(target_col='income')
+        self.assertEqual(len(X_train) + len(X_test), len(self.data_processing.data))
+        self.assertEqual(len(y_train) + len(y_test), len(self.data_processing.data))
+
+        # Balance data
+        balanced_data = self.data_processing.balance_data(target_col='income')
+        self.assertIsInstance(balanced_data, pd.DataFrame)
+        self.assertTrue('income' in balanced_data.columns)
+        self.assertEqual(balanced_data['income'].value_counts().iloc[0], balanced_data['income'].value_counts().iloc[1])
 
 if __name__ == '__main__':
     unittest.main()
